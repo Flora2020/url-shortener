8226ba2d8a365a1d27b70cd5f46c8bb48f51b1f6
Flora2020 <rossignols1989@gmail.com>
2021-02-06 15:58:40 +0800

    feat: add result page

--- app.js
+++ app.js
@@ -13,6 +13,13 @@ app.get('/', (req, res) => {
   res.render('index')
 })
 
+app.post('/', (req, res) => {
+  const HOST = 'http://localhost:3000/'
+  const code = 'test'
+  const url = HOST + code
+  res.render('result', { url })
+})
+
 app.listen(port, () => {
   console.log(`express is listening on http://localhost:${port}`)
 })
