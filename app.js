ee44a5045a561bce53cbdffb57e6830c13db08d3
Flora2020 <rossignols1989@gmail.com>
2021-02-07 16:57:51 +0800

    feat: add url shorten function

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 
+const Url = require('./models/url.js')
+const randomStringGenerate = require('./randomStringGenerate.js')
 require('./config/mongodb.js')
 
 const app = express()
@@ -10,16 +13,34 @@ app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
   res.render('index')
 })
 
 app.post('/', (req, res) => {
-  const HOST = 'http://localhost:3000/'
-  const code = 'test'
-  const url = HOST + code
-  res.render('result', { url })
+  Url.find()
+    .lean()
+    .then((urls) => {
+      let short = randomStringGenerate(5)
+      //避免短網址重複
+      while (urls.some((url) => url.short === short)) {
+        short = randomStringGenerate(5)
+      }
+      Url.create({
+        id: 1,
+        long: req.body.url,
+        short: short
+      })
+        .then(() => {
+          const HOST = 'http://localhost:3000/'
+          const url = HOST + short
+          res.render('result', { url })
+        })
+        .catch(error => console.log(error))
+    })
+    .catch(error => console.log(error))
 })
 
 app.listen(port, () => {
---
+++ randomStringGenerate.js
@@ -0,0 +1,13 @@
+function randomStringGenerate(length) {
+  const alphabet = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+  let result = ''
+  for (let i = 0; i < length; i++) {
+    const index = Math.floor(Math.random() * alphabet.length)
+    result += alphabet[index]
+  }
+
+  return result
+}
+
+module.exports = randomStringGenerate
